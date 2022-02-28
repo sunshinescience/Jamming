4dc1adbcd224c7f450774d1b072b7605ae4d4088
Sunshine Abbott <sunshine.abbott@gmail.com>
2022-02-28 06:23:53 +0000

    Updated the state of searchResults with the value resolved from Spotify.search()‘s promise

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -63,7 +63,9 @@ class App extends React.Component {
   }
 
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults});
+    })
   }
 
   render() {
